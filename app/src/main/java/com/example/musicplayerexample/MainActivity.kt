6a7a5c5d01221e56eeea0adc1e97768045685c03
Josue <josue.ardon113@gmail.com>
2019-07-13 15:43:58 -0600

    Check external storage permission

--- app/src/main/java/com/example/musicplayerexample/MainActivity.kt
+++ app/src/main/java/com/example/musicplayerexample/MainActivity.kt
@@ -1,12 +1,54 @@
 package com.example.musicplayerexample
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.musicplayerexample.utils.Cons
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkExternalStoragePermission()
+
+    }
+
+    private fun checkExternalStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                //
+            } else {
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                    Cons.MY_PERMISSIONS_RQUEST_READ_EXTERNAL_STORAGE)
+            }
+
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            Cons.MY_PERMISSIONS_RQUEST_READ_EXTERNAL_STORAGE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted", Toast.LENGTH_LONG).show()
+
+                } else {
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show()
+                }
+
+                return
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/musicplayerexample/utils/Cons.kt
@@ -0,0 +1,7 @@
+package com.example.musicplayerexample.utils
+
+object Cons {
+
+    val MY_PERMISSIONS_RQUEST_READ_EXTERNAL_STORAGE = 0
+
+}
